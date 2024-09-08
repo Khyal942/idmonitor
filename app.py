0f21b08a6d89fba15d34bb853a71aeeb9cfb6aa3
Khyal942 <khyal610@gmail.com>
2024-09-08 12:29:56 +0530

    app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def stop():
     return redirect(url_for('index'))
 
 
-if __name__ == "__main__":
+        if __name__ == "__main__":
         # app.run(debug=True)  # Comment out or remove this line
 
 
