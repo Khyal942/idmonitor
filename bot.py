5824ae3c2e074be5fe0247161501b13c68312d22
Khyal942 <khyal610@gmail.com>
2024-09-09 12:54:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,26 +6,25 @@ from sql import create_db, add_user, update_user, get_users, delete_user_by_user
 import random
 import time
 
+# Initialize environment variables
 API_ID = os.environ.get("API_ID")
 API_HASH = os.environ.get("API_HASH")
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
-DATABASE_FILE = os.environ.get("users_data.db")
+# Database file (ensure this path is correct)
+DATABASE_FILE = os.environ.get("DATABASE_FILE", "users_data.db")
 
-Telegram = Client(
-    "Telegram",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN
-)
+# Initialize the Telegram client
+Telegram = Client("Telegram", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
+# Ensure the database is created
+create_db()
 
-
+# Global variables
 monitoring_active = False
-delete_on_command = False
 chat_ids = [1716718736, 1656690215]
-
-create_db()  # Ensure this is called to create the database
+notified_users = {}
+last_notification_time = {}
 
 async def send_notification(chat_id, message):
     try:
@@ -33,79 +32,43 @@ async def send_notification(chat_id, message):
     except Exception as e:
         print(f"Error sending notification: {str(e)}")
 
-# Set to keep track of users who have been notified about missing usernames
-
-
-notified_users = {}
-last_notification_time = {}
-
 async def monitor_usernames(chat_id):
     try:
         while monitoring_active:
-            users = get_users(chat_id)  # Get users for the specific chat_id
+            users = get_users(chat_id)
             current_time = time.time()
             for user_id, username, last_known_username, not_found in users:
                 try:
-                    # Debounce: Add a small delay to prevent rapid re-triggering
                     await asyncio.sleep(0.5)
-
                     user_info = await Telegram.get_chat(user_id)
                     current_username = user_info.username if user_info.username else None
                     user_name = user_info.first_name or ""
                     if user_info.last_name:
                         user_name += " " + user_info.last_name
 
-                    # Check for missing username
                     if current_username is None:
                         if user_id not in notified_users or notified_users[user_id] != "missing":
-                            print(f"User ID {user_id} ({user_name}) currently does not have a username set.")
                             notified_users[user_id] = "missing"
-                            await send_notification(chat_id,
-                                                    f"User ID {user_id} ({user_name}) currently does not have a username set.")
+                            await send_notification(chat_id, f"User ID {user_id} ({user_name}) currently does not have a username set.")
                         update_user(user_id, None, True)
-                        continue
-
-                    # Check for username change
-                    if current_username != last_known_username:
-                        # Ensure this is not a repeat of the same change
+                    elif current_username != last_known_username:
                         if (user_id not in notified_users or notified_users[user_id] != current_username):
-
-                            print(f'User ID {user_id} ({user_name}) changed username from @{last_known_username} to @{current_username}.')
-                            await send_notification(chat_id,
-                                                    f'User ID {user_id} ({user_name}) changed username from @{last_known_username} to @{current_username}.')
-
+                            await send_notification(chat_id, f'User ID {user_id} ({user_name}) changed username from @{last_known_username} to @{current_username}.')
                             last_notification_time[user_id] = current_time
                             notified_users[user_id] = current_username
-
-                        # Update the stored username
                         update_user(user_id, current_username, False)
-
                 except Exception as user_error:
                     error_message = str(user_error)
                     if "FLOOD_WAIT" in error_message:
-                        wait_time = 0
-                        try:
-                            wait_time = int(error_message.split('FLOOD_WAIT_')[1].split(' ')[0])
-                        except (ValueError, IndexError):
-                            print(f"Error parsing wait time from error message: {error_message}")
+                        wait_time = int(error_message.split('FLOOD_WAIT_')[1].split(' ')[0]) if 'FLOOD_WAIT_' in error_message else 0
                         if wait_time > 0:
-                            print(f"Rate limit exceeded. Waiting for {wait_time} seconds.")
                             await asyncio.sleep(wait_time)
-                    elif "USERNAME_NOT_OCCUPIED" in error_message:
-                        print(f"Error checking username for user ID {user_id}: Username not occupied.")
+                    elif "USERNAME_NOT_OCCUPIED" in error_message or "USERNAME_INVALID" in error_message:
                         update_user(user_id, last_known_username, True)
-                    elif "USERNAME_INVALID" in error_message:
-                        print(f"Error checking username for user ID {user_id}: Username is invalid.")
-                        update_user(user_id, last_known_username, True)
-                    else:
-                        print(f"Error checking username for user ID {user_id}: {error_message}")
-
             await asyncio.sleep(random.uniform(60, 80))
-
     except Exception as e:
         await send_notification(chat_id, f"Error during monitoring: {str(e)}")
         await asyncio.sleep(80)
-
 
 async def restart_monitoring():
     global monitoring_active
@@ -122,7 +85,7 @@ async def restart_monitoring():
 async def start_monitoring(_, update):
     global monitoring_active
     monitoring_active = True
-    chat_id = update.chat.id  # Get the chat ID where the command was issued
+    chat_id = update.chat.id
     await update.reply_text("Monitoring started. I will notify you of any username changes.")
     await send_notification(chat_id, "Monitoring started.")
     for chat_id in chat_ids:
@@ -143,8 +106,7 @@ async def add_user_command(_, update):
         await update.reply_text("Please provide at least one username or user ID. Usage: /adduser @username1 6707409905 ...")
         return
 
-    users_to_add = update.command[1:]  # Get the list of usernames/user IDs
-
+    users_to_add = update.command[1:]
     for user in users_to_add:
         user = user.strip('@')
         try:
@@ -157,16 +119,14 @@ async def add_user_command(_, update):
                 user_info = await Telegram.get_chat(username)
                 user_id = user_info.id
 
-            chat_id = update.chat.id  # Get the chat ID where the command was issued
+            chat_id = update.chat.id
 
-            # Check if the user ID already exists
             existing_users = get_users(chat_id=chat_id)
             if any(user_id == existing_user_id for existing_user_id, _, _, _ in existing_users):
                 await update.reply_text(f"User with ID `{user_id}` already exists.")
             else:
                 add_user(user_id, username, username, False, chat_id)
                 await update.reply_text(f"User @{username or user_id} added with ID: `{user_id}`")
-
         except Exception as e:
             await update.reply_text(f"Error adding user {user}: {e}")
 
@@ -174,33 +134,24 @@ async def add_user_command(_, update):
 async def show_user_list(_, update):
     user_list_text = "List of added users:\n"
     for chat_id in chat_ids:
-        users = get_users(chat_id=chat_id)  # Fetch users from the database for the given chat ID
+        users = get_users(chat_id=chat_id)
         if users:
             user_list = [f"Chat ID: {chat_id}, ID: {user_id}, Username: @{last_known_username or 'None'}" for user_id, username, last_known_username, not_found in users]
-
             user_list_text += "\n".join(user_list) + "\n\n"
         else:
             user_list_text += f"No users added for chat ID {chat_id}.\n\n"
 
-    # Maximum message length for Telegram
     max_message_length = 4096
-
-    # Split the user list into chunks that fit within the max message length
     messages = []
     current_message = ""
-
     for line in user_list_text.split("\n"):
         if len(current_message) + len(line) + 1 > max_message_length:
             messages.append(current_message)
             current_message = line + "\n"
         else:
             current_message += line + "\n"
-
-    # Append the last message
     if current_message:
         messages.append(current_message)
-
-    # Send all the messages
     for msg in messages:
         await update.reply_text(msg)
 
@@ -218,27 +169,22 @@ async def get_user_id(_, update):
             if user[1] == username:
                 user_id = user[0]
                 break
-
         if user_id:
             await update.reply_text(f"The user ID for @{username} is: `{user_id}`")
         else:
             await update.reply_text(f"User @{username} not found in chat ID {chat_id}. Use /adduser to add them.")
-
     except Exception as e:
         await update.reply_text(f"Error: {e}")
-
 
 @Telegram.on_message(filters.private & filters.command(["delete"]))
 async def delete_user_command(_, update):
     chat_id = update.chat.id
-
     try:
         if len(update.command) < 2:
             await update.reply_text("Please provide a user ID or username. Usage: /delete user_id_or_username")
             return
 
-        user_id_or_username = update.command[1].strip('@')  # Remove the @ symbol if present
-
+        user_id_or_username = update.command[1].strip('@')
         user_id_to_delete = None
         last_known_username_to_delete = None
 
@@ -262,7 +208,6 @@ async def delete_user_command(_, update):
     except Exception as e:
         await update.reply_text(f"Error: {e}")
 
-
 @Telegram.on_message(filters.private & filters.command(["getlist"]))
 async def get_user_list_file(_, update):
     user_list_text = ""
@@ -279,28 +224,21 @@ async def get_user_list_file(_, update):
         await update.reply_text("No users added. Use /adduser to add users.")
         return
 
-    # Define the file path in the current directory
     file_path = os.path.join(os.getcwd(), "added_users_list.txt")
-
-    # Write the content to the text file
     with open(file_path, "w") as file:
         file.write(user_list_text)
 
-    # Send the file
     await update.reply_document(file_path)
-
-    # Clean up the file after sending
     os.remove(file_path)
 
 @Telegram.on_message(filters.private & filters.command(["deletenotfound"]))
-async def delete_user_command(_, update):
-    chat_id = update.chat.id  # Get the chat ID where the command was issued
-    users = get_users(chat_id)  # Pass the chat_id to get_users
+async def delete_not_found_users(_, update):
+    chat_id = update.chat.id
+    users = get_users(chat_id)
     for user_id, username, last_known_username, not_found in users:
         if not_found or last_known_username is None:
             delete_user(user_id)
     await update.reply_text("Usernames not found deleted successfully.")
-
 
 # Start monitoring for all chat IDs
 for chat_id in chat_ids:
